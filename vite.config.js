01d19121423ebf24584067e4b10feb5cbbb6092a
Anshul <sanshul087@gmail.com>
2023-12-11 02:06:10 +0530

    first changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
-  base : "/Personal_portfolio/"
-})
+  build: {
+    // You can add other build configurations here
+
+    // Code splitting configuration
+    chunkSizeWarningLimit: 1200, // Adjust this value as needed
+    output: {
+      manualChunks: {
+        lodash: ['lodash']
+      },
+    },
+  },
+};
